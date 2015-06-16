72a7bd3191b44e4af104117764df7a2666ebd700
shim <shiming@jinlufund.com>
2015-06-16 10:25:52 +0800

    集成配置中心

---
+++ LogObserver-webapp/src/main/java/com/snowstore/log/configuer/PropertiesConfiguration.java
@@ -0,0 +1,18 @@
+package com.snowstore.log.configuer;
+
+import org.springframework.beans.factory.config.PlaceholderConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.snowstore.terra.client.TerraPropertyPlaceholderConfigurer;
+
+@Configuration
+public class PropertiesConfiguration {
+
+	@Bean
+	public PlaceholderConfigurerSupport getTerraPropertyPlaceholderConfigurer() {
+		PlaceholderConfigurerSupport configurerSupport = new TerraPropertyPlaceholderConfigurer();
+		return configurerSupport;
+	}
+
+}
